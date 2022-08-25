4f8cbc48685843d9bb96eeca5239a43c04c7996f
Rokas <rookas.rudzenskas@gmail.com>
2022-08-25 15:58:03 +0300

    Rokas

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,21 +1,36 @@
 import React, {useLayoutEffect} from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, SafeAreaView, Image} from 'react-native';
 import {useNavigation} from "@react-navigation/native";
+import {
+    ChevronDownIcon,
+    UserIcon,
+    SearchIcon,
+    AdjustmentsIcon
+} from "react-native-heroicons/outline";
 
 const HomeScreen = () => {
     const navigation = useNavigation();
     useLayoutEffect(() => {
         navigation.setOptions({
             headerTitle: 'Uizard Wifi Testing',
-            // headerShown: false
+            headerShown: false
         })
     }, [])
     return (
-        <View>
-            <Text className="text-3xl font-bold">
-                Uizard is offline. - user is offline. no connection 🔴
-            </Text>
-        </View>
+        <SafeAreaView>
+            <View className="flex-row pb-3 items-center mx-4 space-x-2">
+                <Image
+                    source={{uri: 'https://links.papareact.com/wru'}}
+                    className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+                />
+                <View>
+                    <Text className="font-bold color-gray-400 text-xs">Deliver Now!</Text>
+                    <Text className="font-bold text-xl">Current Location
+                        <ChevronDownIcon color="#00CCBB" size={20} />
+                    </Text>
+                </View>
+            </View>
+        </SafeAreaView>
     );
 };
 
